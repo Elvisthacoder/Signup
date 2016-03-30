8fd56361445a63d3882d093bcfe3ac6039e59e42
Elvisthacoder <ekk213@gmail.com>
2016-03-30 15:48:13 +0300

    adding the sign up activity

--- app/src/main/java/com/example/taifa/authentification/SignInActivity.java
+++ app/src/main/java/com/example/taifa/authentification/SignInActivity.java
@@ -3,5 +3,18 @@ package com.example.taifa.authentification;
 /**
  * Created by taifa on 3/30/16.
  */
-public class SignInActivity {
+
+import android.os.Bundle;
+import android.app.Activity;
+
+//public class SignInActivity {
+//}
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_screen);
+    }
 }
